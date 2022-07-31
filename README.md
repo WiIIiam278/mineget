1f3aeca6fc070ba57e1c748ba6bbca542533b257
William278 <will27528@gmail.com>
2022-07-31 18:15:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Mineget
+
+Wrapper for a number of Minecraft marketplaces to easily get aggregated resource statistics.
+
+## Usage
+
+Example querying total downloads across multiple marketplaces.
+```js
+const mineget = require('mineget');
+
+// Getting the total downloads of a resource
+// Methods accept an object containing mappings of marketplace IDs
+mineget.downloads({
+    'spigot': 97144, 'polymart': 1634,
+    'songoda': 758
+}).then(result => {
+    console.log(JSON.stringify(result));
+});
+```
+This example will return a json object with following:
+```json
+{
+  "status": "success",
+  "endpoints": {
+    "spigot": {
+      "downloads": 180
+    },
+    "songoda": {
+      "downloads": 2
+    },
+    "polymart": {
+      "downloads": 8
+    }
+  },
+  "total_downloads": 190
+}
+```
+
+## Methods
+
+* `#downloads(ids)` - Get the total downloads of a resource across multiple marketplaces.
+* `#rating(ids)` - Get the average rating and number of ratings of a resource across multiple marketplaces.
+* `#latest_version(ids)` - Get the latest version name and published timestamp of a resource across multiple marketplaces.
+* `#name(ids)` - Get the name of a resource as it is listed on different marketplaces.
+
+## Supported Marketplaces
+You can query the following marketplaces with this module:
+
+- `spigot` - [Spigot](https://www.spigotmc.org/) via [Spiget](https://spiget.org/)
+- `polymart` - [Polymart](https://polymart.com/)
+- `songoda` - [Songoda](https://songoda.com/)
+- `modrinth` - [Modrinth](https://www.modrinth.com/) &dagger;
+
+If you'd like to add support for more marketplaces and additional marketplace API mappings, you can do so by submitting a pull request editing the `marketplaces.json`.
+
+&dagger; *Modrinth IDs are alphanumeric, as opposed to spigot/polymart/songoda which have integer IDs.*
+
+## License
+mineget is licensed under Apache-3.0.
---
+++ mineget.js
@@ -0,0 +1,153 @@
+const {fetchBuilder, MemoryCache} = require('node-fetch-cache');
+const lodash = require('lodash');
+const marketplaces = require('./marketplaces.json');
+const {parseInt} = require("lodash");
+
+// Set up fetching, caching responses for 10 minutes
+const fetch = fetchBuilder.withCache(new MemoryCache({
+    ttl: 60 * 10 * 100
+}));
+
+/**
+ * Query the marketplaces with the specified endpoints using the given ids.
+ * @param ids A map of marketplace name to id. (e.g. {'spigot': '...', 'songoda': '...'})
+ * @param endpoint The endpoint to query. (e.g. 'downloads')
+ * @returns {Promise<unknown>} The result of the query
+ */
+const query = (ids, endpoint) => {
+    return new Promise(async function (res) {
+        let response = {
+            'status': 'success',
+            'endpoints': {}
+        };
+        for (const entry of Object.entries(ids)) {
+            if (!entry) {
+                continue;
+            }
+            let platform = entry[0];
+            let id = entry[1];
+            if (!platform || !id) {
+                continue;
+            }
+            if (!marketplaces.platforms[platform]) {
+                lodash.set(response, `status`, 'error');
+                lodash.set(response, `message`, `Unknown platform ${platform}`);
+                res(response);
+                return;
+            }
+
+            let url = marketplaces.platforms[platform].url;
+            let path = marketplaces.platforms[platform].endpoints[endpoint].endpoint;
+            let fullUrl = (url + path).replace('{id}', id);
+
+            await fetch(fullUrl).then(result => {
+                if (result.status !== 200) {
+                    throw new Error(`Querying ${fullUrl} returned ${result.status}`);
+                }
+                return result.json();
+            }).then(json => {
+                lodash.set(response, `endpoints.${platform}`, {});
+                Object.entries(marketplaces.platforms[platform].endpoints[endpoint].returns).forEach(entry => {
+                    lodash.set(response, `endpoints.${platform}.${entry[0]}`, lodash.get(json, entry[1]));
+                });
+            }).catch(fetchError => {
+                lodash.set(response, `status`, 'error');
+                lodash.set(response, `message`, `Error fetching: ${fetchError}`);
+                res(response);
+                break;
+            });
+        }
+        res(response);
+    });
+}
+
+/**
+ * Query the marketplaces for their download counts and aggregate them together
+ * @param ids A map of marketplace name to id. (e.g. {'spigot': '...', 'songoda': '...'})
+ * @returns {Promise<*>} The result of the query returning a json object, containing the download counts for each marketplace endpoint and the 'total_downloads' count.
+ */
+exports.downloads = function (ids) {
+    let endpointName = 'downloads';
+    return query(ids, endpointName).then(result => {
+        let totalDownloads = 0;
+        Object.entries(result[endpointName]).forEach(entry => {
+            let platformDownloads = parseInt(entry[1][endpointName]);
+            lodash.set(result, `endpoints.${entry[0]}.${endpointName}`, platformDownloads);
+            totalDownloads += platformDownloads;
+        });
+        lodash.set(result, 'total_downloads', totalDownloads);
+        return result;
+    }, error => {
+        return error;
+    });
+}
+
+/**
+ * Query the marketplaces for their star ratings and aggregate them together
+ * @param ids A map of marketplace name to id. (e.g. {'spigot': '...', 'songoda': '...'})
+ * @returns {Promise<*>} The result of the query returning a json object, containing the star ratings for each marketplace endpoint and an aggregated 'average_rating' and 'rating_count'.
+ */
+exports.rating = function (ids) {
+    let endpointName = 'rating';
+    return query(ids, endpointName).then(result => {
+        let totalRating = 0;
+        let totalRatingCount = 0;
+        Object.entries(result[endpointName]).forEach(entry => {
+            let platformRatingAverage = parseFloat(entry[1]['average']);
+            let platformRatingCount = parseInt(entry[1]['count']);
+            lodash.set(result, `endpoints.${entry[0]}.average`, platformRatingAverage);
+            lodash.set(result, `endpoints.${entry[0]}.count`, platformRatingCount);
+            totalRating += platformRatingAverage;
+            totalRatingCount += platformRatingCount;
+        });
+        lodash.set(result, 'average_rating', totalRating / totalRatingCount);
+        lodash.set(result, 'rating_count', totalRatingCount);
+        return result;
+    }, error => {
+        return error;
+    });
+}
+
+/**
+ * Query the marketplaces for the latest version of the resource on each one
+ * @param ids A map of marketplace name to id. (e.g. {'spigot': '...', 'songoda': '...'})
+ * @returns {Promise<*>} The result of the query returning a json object, containing the latest resource version for each marketplace endpoint and the most recently published version as the canonical 'latest_version', alongside 'latest_version_published' indicating when it was published.
+ */
+exports.latest_version = function (ids) {
+    let endpointName = 'latest_version';
+    return query(ids, endpointName).then(result => {
+        let latestVersionName = '';
+        let latestVersionDate = 0;
+        Object.entries(result[endpointName]).forEach(entry => {
+            let platformLatestVersionName = entry[1]['version'];
+            let platformLatestVersionDate = parseInt(entry[1]['published']);
+            lodash.set(result, `endpoints.${entry[0]}.version`, platformLatestVersionName);
+            lodash.set(result, `endpoints.${entry[0]}.published`, parseInt(platformLatestVersionDate));
+            if (platformLatestVersionDate > latestVersionDate) {
+                latestVersionName = platformLatestVersionName;
+                latestVersionDate = platformLatestVersionDate;
+            }
+        });
+        if (latestVersionName !== '') {
+            lodash.set(result, 'latest_version', latestVersionName);
+            lodash.set(result, 'latest_version_published', parseInt(latestVersionDate));
+        }
+        return result;
+    }, error => {
+        return error;
+    });
+}
+
+/**
+ * Query the marketplaces for the name of the resource
+ * @param ids A map of marketplace name to id. (e.g. {'spigot': '...', 'songoda': '...'})
+ * @returns {Promise<*>} The result of the query returning a json object, containing the name of the resource for each marketplace endpoint.
+ */
+exports.name = function (ids) {
+    let endpointName = 'name';
+    return query(ids, endpointName).then(result => {
+        return result;
+    }, error => {
+        return error;
+    });
+}
